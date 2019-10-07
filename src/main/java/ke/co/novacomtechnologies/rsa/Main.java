a608e8ff2684f91646acedcc1a8ea6ac1a874b33
peternjeru <maxp555@gmail.com>
2019-10-07 19:14:50 +0300

    Signature verification

--- src/main/java/ke/co/novacomtechnologies/rsa/Main.java
+++ src/main/java/ke/co/novacomtechnologies/rsa/Main.java
@@ -3,14 +3,12 @@ package ke.co.novacomtechnologies.rsa;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import ke.co.novacomtechnologies.rsa.jackson.CallbackRequest;
 import org.json.JSONObject;
-
-import java.io.IOException;
 import java.security.PublicKey;
 import java.text.DecimalFormat;
 
 public class Main
 {
-    private String callbackRequest = "{\"TransactionType\":\"CustomerPayBillValidation\",\"TransactionTime\":\"20191004115601\",\"TransactionID\":\"NJ45HBN8TH\",\"TransactionAmount\":10,\"BusinessShortcode\":\"107050\",\"AccountReference\":\"acc_ref\",\"SenderMSISDN\":\"254796778039\",\"SenderFirstName\":\"Peter\",\"SenderMiddleName\":null,\"SenderLastName\":\"Test\",\"RemainingCredits\":18,\"Signature\":\"1964fa1a35e5bd88d63e377a5c33f7b0ab328ac8394365ba2864ea46dfae1369b5bb2ee39e5c0eb6d0dc8ab2bbb921c3b1616beefa708325cda1e490f82d8e87c6ea6e242599bb0ca3b5d26fb77961721934182c64dd413a0c97b164aa721b17314ce3e542f82c1bce234e3d2cb41af7ffe22371063a00c4c57b7f47b181ac35\",\"PublicKey\":\"-----BEGIN RSA PUBLIC KEY-----\\r\\nMIGJAoGBAOS8Y8J5FGhSGAxMqfe4vWnHk7wL67IOFW2IvZTzSNx3rmP5HnxCNp5J\\r\\nykX6+JX0+CBthd9+A2eytzX9BtExfz5ziFcdMYFffxSYGGj2vNZmHL0WgxJIe+Yb\\r\\nN3EE+HLYpe0lmVyCABz1zzYsi\\/Rk1ON8qqxwVvpERBcLCsSzLWihAgMBAAE=\\r\\n-----END RSA PUBLIC KEY-----\"}";
+    private String callbackRequest = "{\"TransactionType\":\"CustomerPayBillConfirmation\",\"TransactionTime\":\"20191007081144\",\"TransactionID\":\"NJ78HBNHAA\",\"TransactionAmount\":10,\"BusinessShortcode\":\"107050\",\"AccountReference\":\"acc_ref\",\"SenderMSISDN\":\"254796778039\",\"SenderFirstName\":\"Peter\",\"SenderMiddleName\":null,\"SenderLastName\":\"Test\",\"RemainingCredits\":983,\"Signature\":\"db0d88f2ae43ca244ae8650b151d94cbe40a540ae30df64ae5559cef7866961911b4e802205af561b569833c5bcf710fa9d3cd540d5432a8d31d5a1fc8d02b33b2cef345388d65b7fb26fc9509676ab73eb0d35042e55d5766e2818f35c56b535ce3c7e097417575d3788601d373b6f7cc401f093302a8fcd75becaa2f9cbac1\",\"PublicKey\":\"-----BEGIN RSA PUBLIC KEY-----\\r\\nMIGJAoGBAN4ZwWG3mRtJoBOw\\/z9+\\/\\/z\\/nM6IlFt1vpDeLM0i\\/swZqeV1Jzh8E0KP\\r\\neUHyIvrWJjxsFXI4kbCmhGm44baB4IRkWYc+M7eSWtWTKz12EY0MNdVCed1HqvAy\\r\\n4cNQ1FSEuqQt6Iroe5D8JF5V3+3Pg7IrfW2NVn\\/+BNVidYeyElnfAgMBAAE=\\r\\n-----END RSA PUBLIC KEY-----\"}";
 
     public static void main(String args[]) throws Exception
     {
@@ -25,6 +23,7 @@ public class Main
 
     public boolean plainVerifier() throws Exception
     {
+        System.out.println("\n***Verify by plain JSON Objects***\n");
         JSONObject jsonObject = new JSONObject(callbackRequest);
         String pkJsonString = jsonObject.getString("PublicKey");
         String pkString = pkJsonString
@@ -35,7 +34,8 @@ public class Main
         System.out.println(pkString);
 
         String amount = new DecimalFormat("0.##").format(jsonObject.getDouble("TransactionAmount"));
-        String data = jsonObject.getString("TransactionID")
+        String data = jsonObject.getString("TransactionTime")
+                + jsonObject.getString("TransactionID")
                 + amount
                 + jsonObject.getString("AccountReference")
                 + jsonObject.getString("SenderMSISDN")
@@ -55,6 +55,7 @@ public class Main
 
     public boolean jacksonVerifier() throws Exception
     {
+        System.out.println("\n***Verify by Jackson Pojo Objects***\n");
         ObjectMapper objectMapper = new ObjectMapper();
         CallbackRequest request = objectMapper.readValue(callbackRequest, CallbackRequest.class);
         String pkJsonString = request.getPublicKey();
@@ -66,7 +67,8 @@ public class Main
         System.out.println(pkString);
 
         String amount = new DecimalFormat("0.##").format(request.getTransactionAmount());
-        String data = request.getTransactionID()
+        String data = request.getTransactionTime()
+                + request.getTransactionID()
                 + amount
                 + request.getAccountReference()
                 + request.getSenderMSISDN()
